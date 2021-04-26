ea972ea8894179c94e54058dac166c6e8f179caf
Matthew Jiao <jiaomatthew222@gmail.com>
2021-04-26 18:41:45 -0400

    configure fps

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -1,13 +1,20 @@
 import cv2
-
+import time
 cap = cv2.VideoCapture("PoseVideos/1.mp4")
-
+pTime = 0
 
 while True:
-    success, image = cap.read()
-    height, width, layers = image.shape
+    success, img = cap.read()
+    height, width, layers = img.shape
     new_h = int(height / 2)
     new_w = int(width / 2)
-    resize = cv2.resize(image, (new_w, new_h))
-    cv2.imshow("Image", resize)
+    img = cv2.resize(img, (new_w, new_h))
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow("Image", img)
+
     cv2.waitKey(1)
