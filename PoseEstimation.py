f051f84ee9fba088b831d7bdb2bab7252b4e8a04
Matthew Jiao <jiaomatthew222@gmail.com>
2021-04-26 22:28:30 -0400

    tracking landmark points

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -1,14 +1,31 @@
 import cv2
 import time
+import mediapipe as mp
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
 cap = cv2.VideoCapture("PoseVideos/1.mp4")
 pTime = 0
-
 while True:
     success, img = cap.read()
+
     height, width, layers = img.shape
     new_h = int(height / 2)
     new_w = int(width / 2)
     img = cv2.resize(img, (new_w, new_h))
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            cx, cy = int(lm.x*w), int(lm.y*h)
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
