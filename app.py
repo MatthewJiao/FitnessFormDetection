19c466030bfd742b24119e7f674829f861a6cf7a
Matthew Jiao <jiaomatthew222@gmail.com>
2021-04-28 00:34:56 -0400

    page launch grid

--- app.py
+++ app.py
@@ -146,6 +146,10 @@ def gen_frames():
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
